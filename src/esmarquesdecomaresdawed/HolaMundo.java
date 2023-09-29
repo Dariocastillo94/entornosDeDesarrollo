9ef58268af71f8209fb61d012f328c133c846cc2
Araceli <arareina2003@gmail.com>
2023-09-29 14:19:52 +0200

    fix(chapter2): 1 added

--- src/esmarquesdecomaresdawed/HolaMundo.java
+++ src/esmarquesdecomaresdawed/HolaMundo.java
@@ -3,7 +3,7 @@ package esmarquesdecomaresdawed;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo320");
+		System.out.println("Hola mundo3");
 	}
 
 }
