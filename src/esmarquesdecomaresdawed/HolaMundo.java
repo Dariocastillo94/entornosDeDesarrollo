00cc60c9b7fb653ef76f7f27ba6093703c5eeb30
Darío <dariocastillo94@hotmail.com>
2023-09-29 14:31:54 +0200

    Feat(1)added

--- src/esmarquesdecomaresdawed/HolaMundo.java
+++ src/esmarquesdecomaresdawed/HolaMundo.java
@@ -1,8 +1,11 @@
 package esmarquesdecomaresdawed;
 
+import java.util.Scanner;
+
 public class HolaMundo {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Hola mundo320");
 	}
 
